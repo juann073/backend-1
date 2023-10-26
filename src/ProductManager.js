0491095e08e5dbd9b4486b3d7a9aa1c4e7c6c646
juan losada <juanlosada073@gmail.com>
2023-10-25 23:54:51 -0300

    entrega q

--- src/ProductManager.js
+++ src/ProductManager.js
@@ -1,90 +1,115 @@
-import  Express  from "express"
-import fs from 'fs'
-const app = Express()
-app.get('/productos', (req, res) => res.send(productManager))
-app.listen(8080, ()=>{
-    console.log('Server is runnning...');
-})
+import fs from "fs";
+
 class ProductManager {
-    constructor() {
-        this.products = []
-    
+  constructor(path) {
+    this.products = [];
+    this.path = path;
+  }
+  getNextId = () => {
+    this.sinkDB();
+    const count = this.products.length;
+    const lastEvent = this.products[count - 1];
+    if (count === 0) {
+      return 1;
     }
-    getNextId = () => {
-        const count = this.products.length
-        const lastEvent = this.products[count - 1]
-        if (count === 0) {
-            return 1
-        }
-        return lastEvent.id + 1
+    return lastEvent.id + 1;
+  };
+  sinkDB = () => {
+    const file = fs.existsSync(this.path);
+    if (file) {
+      const data = fs.readFileSync(this.path, "utf-8");
+      this.products = JSON.parse(data);
+    } else {
+      fs.writeFileSync(this.path, JSON.stringify(this.products));
     }
-    getProduct = () => { return this.products }
-    getProductbyId = (id) => {
-        const productFound = this.products.find(product => product.id === id)
-        if (!productFound) {
-            console.error('ese objeto no existe');
-        }
-        return productFound
+  };
+  getProduct = () => {
+    this.sinkDB();
+    return this.products;
+  };
+  getProductbyId = (id) => {
+    this.sinkDB();
+    const productFound = this.products.find((product) => product.id === id);
+    return productFound;
+  };
+  addProduct = (title, description, price, thumbnail, code, stock) => {
+    this.sinkDB();
+    const id = this.getNextId();
+    const product = {
+      id,
+      title,
+      price,
+      description,
+      thumbnail,
+      code,
+      stock,
+    };
+    if (this.products.some((product) => product.code === code)) {
+      return "Code already exists.";
     }
-    addProduct = (title, description, price, thumbnail, stock) => {
-        const id = this.getNextId()
-        const product = {
-            id,
-            title,
-            price,
-            description,
-            thumbnail,
-            stock
-        }
-        this.products.push(product)
+    this.products.push(product);
+    fs.writeFileSync(this.path, JSON.stringify(this.products));
+    return product;
+  };
+  updateProduct = async (
+    id,
+    title,
+    description,
+    price,
+    thumbnail,
+    code,
+    stock
+  ) => {
+    try {
+      const existFile = fs.existsSync(this.path);
+      if (!existFile) {
+        fs.writeFileSync(this.path, JSON.stringify(this.products));
+      } else {
+        const data = fs.readFileSync(this.path, "utf-8");
+        this.products = JSON.parse(data);
+      }
+      const { id } = product;
+      if (!id) {
+        return "invalis ID";
+      }
+      let productUpdate = this.products.find((p) => p.id === id);
+      if (!productUpdate) {
+        return "Not Found.";
+      }
+      let newArray = this.products.filter((p) => p.id !== id);
+      let verifyCode = this.products.find((p) => p.code !== code);
+      if (verifyCode) {
+        return "El code ya existe";
+      }
+      productUpdate = { productUpdate, ...product };
+      newArray.push(productUpdate);
+      this.products = newArray;
+      await fs.promises.writeFile(this.path, JSON.stringify(this.products));
+      return productUpdate
+    } catch (error) {
+      return "error";
     }
-    updateProduct = async (id, title, description, price, thumbnail, stock) => {
-        const update = {
-
-            id,
-
-            title,
-
-            description,
-
-            price,
-
-            thumbnail,
-
-            stock
-
-        };
-        const updatedProduct = { ...this.products[this.products.find(product => product.id === id)], ...update };
-
-        this.products.find(product => product.id === id) = updatedProduct
-
-        await fs.promises.writeFile(this.path, JSON.stringify(this.products));
-
-    }
-    deleteProduct = (id) => {
-        const eliminar = this.products.filter(product => product.id !== id)
-        if (id === eliminar) {
-            console.log('Producto elimininado');
-        } else
-            return eliminar
-    }
-
-
+  };
+  deleteProduct = (id) => {
+    this.sinkDB();
+    const nuevoArray = this.products.filter((product) => product.id !== id);
+    this.products = nuevoArray;
+    fs.writeFileSync(this.path, JSON.stringify(this.products));
+    return "Eliminado correctamente";
+  };
 }
 
-const productManager = new ProductManager()
-productManager.addProduct('celular', 'lorem', 500, 'img.jpg', 10)
-productManager.addProduct('pepitos', 'lorem', 500, 'img.jpg', 10)
-productManager.addProduct('cacao', 'lorem', 500, 'img.jpg', 10)
-productManager.addProduct('galletas', 'lorem', 500, 'img.jpg', 10)
-productManager.addProduct('chupetines', 'lorem', 500, 'img.jpg', 10)
-productManager.updateProduct()
-console.log(productManager.getProduct());
-console.log(productManager.getProductbyId());
-console.log(productManager.updateProduct('loro', 'lorem', 500, 'img.jpg', 10));
-console.log(productManager.updateProduct('carucha', 'lorem', 500, 'img.jpg', 10));
-console.log(productManager.deleteProduct())
+const manager = new ProductManager("./DB.json");
+//manager.addProduct('celular', 'lorem', 500, 'img.jpg', 10)
+// manager.addProduct('pepitos', 'lorem', 500, 'img.jpg', 10)
+// // productManager.addProduct('cacao', 'lorem', 500, 'img.jpg', 10)
+// // productManager.addProduct('galletas', 'lorem', 500, 'img.jpg', 10)
+// // productManager.addProduct('chupetines', 'lorem', 500, 'img.jpg', 10)
+manager.updateProduct(1, 'chupetines', 'lorem', 500, 'img.jpg', 10)
+// console.log(manager.getProduct());
+// // console.log(productManager.getProductbyId());
+// // console.log(productManager.updateProduct('loro', 'lorem', 500, 'img.jpg', 10));
+// // console.log(productManager.updateProduct('carucha', 'lorem', 500, 'img.jpg', 10));
+// // console.log(productManager.deleteProduct())
 
-export default ProductManager
-
-
+export default manager;
--- src/app.js
+++ src/app.js
@@ -1,13 +1,57 @@
-import Express from "express"
-import ProductManager from "./ProductManager"
-const app = Express()
+import Express from "express";
+import ProductManager from "./ProductManager.js";
+const app = Express();
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: true }));
 
-app.get('/', (request,response) => {
-    response.send('Bienvenido')
-})
+app.get("/", async (req, res) => {
+  res.send("Bienvenido");
+});
+app.get("/products", async (req, res) => {
+  try {
+    const products = ProductManager.getProduct();
+    const limit = req.query.limit;
+    if (limit) {
+      return res.send(products.slice(0, limit));
+    }
+    console.log(limit);
+    return res.send(products);
+  } catch (error) {
+    return res.send(error);
+  }
+});
 
-app.listen(8080, ()=>{
-    console.log('Server is runnning...');
-})
+app.get("/products/:pid", async (req, res) => {
+  try {
+    const { pid } = req.params;
+    console.log(req.params);
+    const product = ProductManager.getProductbyId(Number(pid));
+    if (!product) {
+      return res.status(404).send({ error: "Product Not Found" });
+    }
+    return res.send(product);
+  } catch (error) {
+    console.log(error);
+    return res.send(error);
+  }
+});
+app.put("/products/:pid", async (req, res) => {
+    try {
+      const { pid } = req.params;
+      console.log(req.params);
+      const product = ProductManager.updateProduct(Number(pid));
+      if (!product) {
+        return res.status(404).send({ error: "Product Not Found" });
+      }
+      return res.send(product);
+    } catch (error) {
+      console.log(error);
+      return res.send(error);
+    }
+  });
 
-export default app
+app.listen(8080, () => {
+  console.log("Server is runnning...");
+});
+
+export default app;
